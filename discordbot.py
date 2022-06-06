98afaab4cfe4c464cb92c3153b1b2f4287d78d2e
Go Tanioka <emf00657@gmail.com>
2022-06-06 21:26:35 +0900

    event

--- discordbot.py
+++ discordbot.py
@@ -11,13 +11,6 @@ voicevox_speaker = os.getenv("VOICEVOX_SPEAKER", default="14")
 token = os.getenv("DISCORD_BOT_TOKEN")
 
 
-def bot_voice_client():
-    voices = client.voice_clients
-    for vc in voices:
-        if vc.user.id == client.user.id:
-            return vc
-
-
 def load_words():
     with open("./goroku.txt", "r", encoding="utf-8") as f:
         lines = f.readlines()
@@ -33,14 +26,6 @@ async def member_voice_play(member, text, intonation=1, speed=0.9):
         await asyncio.sleep(0.5)
     source = await discord.FFmpegOpusAudio.from_probe(mp3url)
     member.guild.voice_client.play(source)
-
-
-async def voice_client_play(vc, text, intonation=1, speed=0.9):
-    mp3url = f"https://api.su-shiki.com/v2/voicevox/audio/?text={text}&key={voicevox_key}&speaker={voicevox_speaker}&intonationScale={intonation}&speed={speed}"
-    while vc.is_playing():
-        await asyncio.sleep(0.5)
-    source = await discord.FFmpegOpusAudio.from_probe(mp3url)
-    vc.play(source)
 
 
 @client.event
@@ -84,14 +69,13 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
-async def reply(message):
-    await member_voice_play(bot_voice_client(), message.content)
-
-
-@client.listen
+@client.event
 async def on_message(message):
     if client.user in message.mentions:
-        await reply(message)
+        for member in client.get_all_members():
+            if member.user.id == client.user.id:
+                await member_voice_play(member, message.content)
+                return
 
 
 @client.command()
